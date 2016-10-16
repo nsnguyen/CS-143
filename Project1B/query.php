98a680dc4f87aae329b60fc9d845e3cc72d5271b
nsnguyen <nsnguyen@g.ucla.edu>
2016-10-16 14:06:14 -0700

    fixed query php

--- Project1B/query.php
+++ Project1B/query.php
@@ -11,17 +11,15 @@
         <textarea name="query" cols="60" rows="10"></textarea><br />
         <input type="submit" value="Submit" />
     </form>
-
+    <table border=1 cellspacing=0 cellpadding=1>
+        <tr>
     <?php
 
     $sql = $_GET["query"];
-
     $servername = "localhost";
     $username = "cs143";
     $password = "";
     $database = "TEST";
-
-    $test = "SELECT * FROM Movie";
 
     if(!$conn = mysql_connect($servername,$username,$password)){
         echo "Could not connect to mySQL server.";
@@ -29,36 +27,29 @@
     }
 
     if(!mysql_select_db($database,$conn)){
-        echo "Could not select database";
+        echo "Could not select database.";
         exit;
     }
 
     $result = mysql_query($sql,$conn);
 
-
     if(!result){
-        echo "DB Error, Could not query the database\n";
+        echo "DB Error, Could not query the database.\n";
         echo "mySQL Error: " . mysql_error();
         exit;
     }
 
-    $i = 0;
-    echo '<table border=1 cellspacing=1 cellpadding=2><tr>';
-    while ($i < mysql_num_fields($result)){
-        $meta = mysql_fetch_field($result,$i);
-        echo '<td><b>' . $meta->name . '</b></td>';
-        $i = $i + 1;
+    #Getting attribute names.
+    $numFields = mysql_numfields($result);
+    for($i=0; $i < $numFields; $i++){
+        $obj = mysql_fetch_field($result,$i);
+        echo '<td><b>' . $obj->name . '</b></td>';
     }
-
     echo '<tr>';
 
-    $x = 0;
-
-
-    # read a row
-    while ($row = mysql_fetch_row($result)){
-        # for each element in that row
-        for($x=0; $x<$i; $x++){
+    #fetching data each row.
+    while($row = mysql_fetch_row($result)){
+        for($x=0; $x < $numFields; $x++){
             if ($row[$x] == NULL){
                 echo '<td>N/A</td>';}
             else{
@@ -69,11 +60,15 @@
     }
     echo '</tr></table>';
 
-
+    #free result and close connection.
     mysql_free_result($result);
-    #mysql_close($conn);
+    mysql_close($conn);
 
     ?>
+
+
+        </tr>
+    </table>
 </body>
 
 </html>
