51f0acf98486d6aed6f6a497148884349b0b4b44
nsnguyen <nsnguyen@g.ucla.edu>
2016-10-22 15:49:05 -0700

    changed logic around

--- Project1B/query.php
+++ Project1B/query.php
@@ -8,7 +8,7 @@
     <h5>Created by Nguyen Si Nguyen UID: 004870721</h5>
 
     <h4>Please note that the query is case sensitive.</h4>
-    <h5>Example Query to show all attributes for first 5 tuples: SELECT * FROM Movie LIMIT 5;</h5>
+    <h5>Example Query: Show all attributes for first 5 tuples: SELECT * FROM Movie LIMIT 5;</h5>
     <form method="GET">
         <textarea align="left" name="query" cols="60" rows="10" placeholder="Type in a query..."><?php
             $sql = trim($_GET["query"]);
@@ -25,17 +25,18 @@
 
     $mysqli = new mysqli($server,$user,$psswrd,$database);
 
-    if($mysqli-> connect_errno){
+    if($mysqli-> connect_errno){ #Check for connection error.
         printf($mysqli->connect_error);
         exit();
     }
 
-    if(!$mysqli->query($sql)){
+    $result =  $mysqli->query($sql);
+
+    if(!$result){ #Check if query has an error.
         printf($mysqli->error);
         exit();
     }
-
-    if($result = $mysqli->query($sql)) {
+    else{
         printf('<table border=1 cellspacing=1 cellpadding=1><tr>');
 
         #print headers in bold
@@ -56,6 +57,7 @@
         #close connection.
         $mysqli->close();
     }
+
     ?>
 </body>
 </html>
